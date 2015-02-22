2df6714585bb5821473a244d9a5af0e116d12452
mamogmx <marco.carbone@gmx.com>
2015-02-22 06:28:54 +0100

    Added .gitignore

--- lib/wsPraticawebBase.php
+++ lib/wsPraticawebBase.php
@@ -1,6 +1,7 @@
 <?php
 require_once DIR."lib/nusoap/nusoap.php";
 require_once DIR.'lib/utils.php';
+
 $dbh=new PDO(DSN);
 $schema="iol";
 $server = new nusoap_server; 
@@ -10,12 +11,47 @@ $server->configureWSDL('praticaweb', 'http://webservice.gisweb.it/wspraticaweb/s
 
 $server->wsdl->addSimpleType('tipopratica','xsd:string','SimpleType','scalar',array_keys($tipoPratica));
 $server->wsdl->addSimpleType('tipointervento','xsd:string','SimpleType','scalar',array_keys($tipoIntervento));
-/*
-$server->addComplexType('procedimento','complexType','struct','all','',Array(
-    "tipo"=>Array("name"=>"tipo","type"=>"tns:tipopratica"),
-    "intervento"=>Array("name"=>"tipo","type"=>"tns:tipointervento"),
-    
-));*/
+
+$server->wsdl->addComplexType('procedimento','complexType','struct','all','',Array(
+    "anno" => Array("name"=>"anno","type"=>"xsd:int"),
+    "aut_amb" => Array("name"=>"aut_amb","type"=>"xsd:string"),
+    "cartella" => Array("name"=>"cartella","type"=>"xsd:string"),
+    "categoria" => Array("name"=>"categoria","type"=>"xsd:string"),
+    "com_resp" => Array("name"=>"com_resp","type"=>"xsd:string"),
+    "data_chiusura" => Array("name"=>"data_chiusura","type"=>"xsd:string"),
+    "data_chiusura_pa" => Array("name"=>"data_chiusura_pa","type"=>"xsd:string"),
+    "data_com_resp" => Array("name"=>"data_com_resp","type"=>"xsd:string"),
+    "data_presentazione" => Array("name"=>"data_presentazione","type"=>"xsd:string"),
+    "data_prot" => Array("name"=>"data_prot","type"=>"xsd:string"),
+    "data_prot_int" => Array("name"=>"data_prot_int","type"=>"xsd:string"),
+    "data_resp" => Array("name"=>"data_resp","type"=>"xsd:string"),
+    "data_resp_ia" => Array("name"=>"data_resp_ia","type"=>"xsd:string"),
+    "data_resp_it" => Array("name"=>"data_resp_it","type"=>"xsd:string"),
+    "diritti_segreteria" => Array("name"=>"diritti_segreteria","type"=>"xsd:string"),
+    "intervento" => Array("name"=>"intervento","type"=>"xsd:string"),
+    "note" => Array("name"=>"note","type"=>"xsd:string"),
+    "note_chiusura" => Array("name"=>"note_chiusura","type"=>"xsd:string"),
+    "note_chiusura_pa" => Array("name"=>"note_chiusura_pa","type"=>"xsd:string"),
+    "numero" => Array("name"=>"numero","type"=>"xsd:string"),
+    "oggetto" => Array("name"=>"oggetto","type"=>"xsd:string"),
+    "online" => Array("name"=>"online","type"=>"xsd:string"),
+    "pagamento_diritti" => Array("name"=>"pagamento_diritti","type"=>"xsd:string"),
+    "pratica" => Array("name"=>"pratica","type"=>"xsd:string"),
+    "prog" => Array("name"=>"prog","type"=>"xsd:string"),
+    "protocollo" => Array("name"=>"protocollo","type"=>"xsd:string"),
+    "protocollo_int" => Array("name"=>"protocollo_int","type"=>"xsd:string"),
+    "resp_ia" => Array("name"=>"resp_ia","type"=>"xsd:string"),
+    "resp_it" => Array("name"=>"resp_it","type"=>"xsd:string"),
+    "resp_proc" => Array("name"=>"resp_proc","type"=>"xsd:string"),
+    "riduzione_diritti" => Array("name"=>"riduzione_diritti","type"=>"xsd:string"),
+    "rif_aut_amb" => Array("name"=>"rif_aut_amb","type"=>"xsd:string"),
+    "riferimento" => Array("name"=>"riferimento","type"=>"xsd:string"),
+    "riferimento_to" => Array("name"=>"riferimento_to","type"=>"xsd:string"),
+    "rif_pratica" => Array("name"=>"rif_pratica","type"=>"xsd:string"),
+    "tipo" => Array("name"=>"tipo","type"=>"xsd:string")
+));
+
+
 
 $server->wsdl->addComplexType('soggetto','complexType','struct','all','',Array(
     "albo"=>Array("name"=>"albo","type"=>"xsd:string"),
@@ -80,14 +116,14 @@ $server->wsdl->addComplexType('particella','complexType','struct','all','',Array
     )
 );
 $server->wsdl->addComplexType('indirizzo','complexType','struct','all','',Array(
-        "sezione"=>Array("name"=>"via","type"=>"xsd:string"),
-        "foglio"=>Array("name"=>"civico","type"=>"xsd:string"),
-        "mappale"=>Array("name"=>"interno","type"=>"xsd:string")
+        "via"=>Array("name"=>"via","type"=>"xsd:string"),
+        "civico"=>Array("name"=>"civico","type"=>"xsd:string"),
+        "interno"=>Array("name"=>"interno","type"=>"xsd:string")
         
     )
 );
 
-$server->wsdl->addComplexType('indirizzi','simpleType','array','all','',Array("indirizzo"=>Array("name"=>"indirizzo","type"=>"tns:indirizzo")));
+$server->wsdl->addComplexType('indirizzi','complexType','array','all','',Array("indirizzo"=>Array("name"=>"indirizzo","type"=>"tns:indirizzo")));
 $server->wsdl->addComplexType('soggetti','simpleType','array','all','',Array("soggetti"=>Array("name"=>"soggetto","type"=>"tns:soggetto")));
 $server->wsdl->addComplexType('particelleterreni','simpleType','array','all','',Array("particella"=>Array("name"=>"particella","type"=>"tns:particella")));
 $server->wsdl->addComplexType('particelleurbano','simpleType','array','all','',Array("particella"=>Array("name"=>"particella","type"=>"tns:particella")));
@@ -99,14 +135,9 @@ $server->wsdl->schemaTargetNamespace = 'urn:praticaweb';
 
 $server->register('aggiungiPratica',
     Array(
-        "tipo"=>"tns:tipopratica",
-        "intervento"=>"tns:tipointervento",
-        "oggetto"=>"xsd:string",
-        "note"=>"xsd:string",
-        "data_presentazione"=>"xsd:string",
-        "numero_protocollo"=>"xsd:string",
-        "data_protocollo"=>"xsd:string",
-        "responsabile_procedimento"=>"xsd:string"
+        "procedimento"=>"tns:procedimento",
+        "indirizzi"=>"tns:indirizzi"
+        
     ),
     Array(
         "success"=>"xsd:int",
@@ -137,24 +168,79 @@ $server->register('aggiungiSoggetto',
     'encoded',
     'Metodo che aggiunge un soggetto ad pratica edilizia al software Praticaweb 2.0'
 );
-
-function aggiungiPratica($tipo,$intervento,$oggetto,$note,$data_pres,$prot,$data_prot,$resp_proc){
+$server->register('aggiungiIndirizzo',
+    Array(
+        "pratica"=>"xsd:int",
+        "soggetto"=>"tns:indirizzo"
+    ),
+    Array(
+        "success"=>"xsd:int",
+        "message"=>"xsd:string",
+        "idindirizzo"=>"xsd:int"
+    ),
+    'urn:praticaweb',
+    'urn:praticaweb#addIndirizzo',
+    'rpc',
+    'encoded',
+    'Metodo che aggiunge un indirizzo ad pratica edilizia al software Praticaweb 2.0'
+);
+$server->register('aggiungiCatastoUrbano',
+    Array(
+        "pratica"=>"xsd:int",
+        "soggetto"=>"tns:particella"
+    ),
+    Array(
+        "success"=>"xsd:int",
+        "message"=>"xsd:string",
+        "idpartic"=>"xsd:int"
+    ),
+    'urn:praticaweb',
+    'urn:praticaweb#addCatastoUrbano',
+    'rpc',
+    'encoded',
+    'Metodo che aggiunge un CU ad pratica edilizia al software Praticaweb 2.0'
+);
+$server->register('aggiungiCatastoTerreni',
+    Array(
+        "pratica"=>"xsd:int",
+        "soggetto"=>"tns:particella"
+    ),
+    Array(
+        "success"=>"xsd:int",
+        "message"=>"xsd:string",
+        "idiparticella"=>"xsd:int"
+    ),
+    'urn:praticaweb',
+    'urn:praticaweb#addCatastoTerreni',
+    'rpc',
+    'encoded',
+    'Metodo che aggiunge un CT ad pratica edilizia al software Praticaweb 2.0'
+);
+function aggiungiPratica($procedimento,$indirizzi=[]){
     $dbh=new PDO(DSN);
     $schema="pe";
-    $data = Array("tipo"=>$tipo,"intervento"=>$intervento,"oggetto"=>$oggetto,"note"=>$note,"data_presentazione"=>$data_pres,"protocollo"=>$prot,"data_prot"=>$data_prot,"resp_proc"=>$resp_proc);
+    $params = Array("tipo","intervento","oggetto","note","data_presentazione","protocollo","data_prot","resp_proc","online");
+    //$data = Array("tipo"=>$tipo,"intervento"=>$intervento,"oggetto"=>$oggetto,"note"=>$note,"data_presentazione"=>$data_pres,"protocollo"=>$prot,"data_prot"=>$data_prot,"resp_proc"=>$resp_proc);
+    foreach($params as $key){
+        $data[$key]=($procedimento[$key])?($procedimento[$key]):(null);
+    }
     
     utils::debug(utils::debugDir."aggiungiPratica.debug", $data);
+    utils::debug(utils::debugDir."Indirizzi.debug", $indirizzi);
+
     $result=Array();
-    $sql="INSERT INTO $schema.avvioproc(tipo,intervento,oggetto,note,data_presentazione,protocollo,data_prot,resp_proc) values(:tipo,:intervento,:oggetto,:note,:data_presentazione,:protocollo,:data_prot,:resp_proc)";
+    $sql="INSERT INTO $schema.avvioproc(tipo,intervento,oggetto,note,data_presentazione,protocollo,data_prot,resp_proc,online) values(:tipo,:intervento,:oggetto,:note,:data_presentazione,:protocollo,:data_prot,:resp_proc,:online)";
     //$dbh->beginTransaction();
     $stmt=$dbh->prepare($sql);
    
-    if (!$stmt->execute($data))
+    if (!$stmt->execute($data)){
         $errors=$stmt->errorInfo();
+        utils::debug(utils::debugDir."error-aggiungiPratica.debug", $errors);
+    }
     else{
         $pratica=$dbh->lastInsertId("pe.avvioproc_id_seq");
-        $sql="UPDATE pe.avvioproc SET pratica=id WHERE id =$pratica;";
-        $dbh->query($sql);
+        //$sql="UPDATE pe.avvioproc SET pratica=id WHERE id =$pratica;";
+        //$dbh->query($sql);
         
         $sql="SELECT * FROM pe.avvioproc WHERE pratica=:pratica";
         
@@ -162,10 +248,11 @@ function aggiungiPratica($tipo,$intervento,$oggetto,$note,$data_pres,$prot,$data
         
         $stmt->execute(Array("pratica"=>$pratica));
         $res = $stmt->fetch(PDO::FETCH_ASSOC);
+        $numero_pratica = $res["numero"];
     }
-    /*for($i=0;$i<count($soggetti);$i++){
-        $res=aggiungiSoggetto($pratica,$soggetti[$i]);
-    }*/
+    for($i=0;$i<count($indirizzi);$i++){
+        $res=aggiungiIndirizzo($pratica,$indirizzi[$i]);
+    }
     
     
     if ($errors){
@@ -173,10 +260,8 @@ function aggiungiPratica($tipo,$intervento,$oggetto,$note,$data_pres,$prot,$data
         $result=Array("success"=>"-1","message"=>$errors[2]);
     }
     else{
-        
-        
         //$dbh->commit();
-        $result = Array("success"=>1,"message"=>"OK","pratica"=>$pratica,"numero_pratica"=>$res["numero"]);
+        $result = Array("success"=>1,"message"=>"OK","pratica"=>$pratica,"numero_pratica"=>$numero_pratica);
     }
     return $result;
 }
@@ -220,27 +305,103 @@ function rimuoviSoggetto($pratica,$soggetto){
     $result=Array();
     return $result;
 }
-function aggiungiParticellaTerreni($pratica,$particella_terreni){
+function aggiungiCatastoTerreni($pratica,$ct){
+    $dbh=new PDO(DSN);
+    $schema="pe";
+    $params = Array("foglio","mappale");
+    foreach($params as $key){
+        $data[$key]=($ct[$key])?($ct[$key]):(null);
+    }
+    $data["pratica"]=$pratica;
+    utils::debug(utils::debugDir."aggiungiTerreni.debug", $ct);
     $result=Array();
-    $sql="INSERT INTO $schema.cterreni(foglio,mappale,sezione,sub) VALUES(:foglio,:mappale,:sezione,:sub);";
+    $sql="INSERT INTO $schema.cterreni(pratica,foglio,mappale) VALUES(:pratica,:foglio,:mappale);";
+    $stmt=$dbh->prepare($sql);
+   
+    if (!$stmt->execute($data)){
+        $errors=$stmt->errorInfo();
+        utils::debug(utils::debugDir."error-aggiungiTerreni.debug", $errors);
+    }
+    else{
+        $idpartic=$dbh->lastInsertId("pe.cterreni_id_seq");
+    }
+    
+    if ($errors){
+        //$dbh->rollBack();
+        $result=Array("success"=>"-1","message"=>$errors[2]);
+    }
+    else{
+        $result = Array("success"=>1,"message"=>"OK","idpartic"=>$idpartic);
+    }
     return $result;
 }
 function rimuoviParticellaTerreni($pratica,$particella_terreni){
     $result=Array();
     return $result;
 }
-function aggiungiParticellaUrbano($pratica,$particella_urbano){
+function aggiungiCatastoUrbano($pratica,$ct){
+    $dbh=new PDO(DSN);
+    $schema="pe";
+    $params = Array("foglio","mappale","sub");
+    foreach($params as $key){
+        $data[$key]=($ct[$key])?($ct[$key]):(null);
+    }
+    $data["pratica"]=$pratica;
+    utils::debug(utils::debugDir."aggiungiTerreni.debug", $data);
     $result=Array();
-    $sql="INSERT INTO $schema.cterreni(foglio,mappale,sezione,sub) VALUES(:foglio,:mappale,:sezione,:sub);";
+    $sql="INSERT INTO $schema.curbano(pratica,foglio,mappale,sub) VALUES(:pratica,:foglio,:mappale,:sub);";
+    $stmt=$dbh->prepare($sql);
+   
+    if (!$stmt->execute($data)){
+        $errors=$stmt->errorInfo();
+        utils::debug(utils::debugDir."error-aggiungiUrbano.debug", $errors);
+    }
+    else{
+        $idpartic=$dbh->lastInsertId("pe.curbano_id_seq");
+    }
+    
+    if ($errors){
+        //$dbh->rollBack();
+        $result=Array("success"=>"-1","message"=>$errors[2]);
+    }
+    else{
+        $result = Array("success"=>1,"message"=>"OK","idpartic"=>$idpartic);
+    }
     return $result;
 }
+
 function rimuoviParticellaUrbano($pratica,$particella_urbano){
     $result=Array();
     return $result;
 }
 function aggiungiIndirizzo($pratica,$indirizzo){
+    $dbh=new PDO(DSN);
+    $schema="pe";
+    $params = Array("via","civico","interno");
+    foreach($params as $key){
+        $data[$key]=($indirizzo[$key])?($indirizzo[$key]):(null);
+    }
+    $data["pratica"]=$pratica;
+    utils::debug(utils::debugDir."aggiungiIndirizzo.debug", $data);
     $result=Array();
-    $sql="INSERT INTO $schema.indirizzi(civico,interno,via) VALUES(:civico,:interno,:via);";
+    $sql="INSERT INTO $schema.indirizzi(pratica,civico,interno,via) VALUES(:pratica,:civico,:interno,:via);";
+    $stmt=$dbh->prepare($sql);
+   
+    if (!$stmt->execute($data)){
+        $errors=$stmt->errorInfo();
+        utils::debug(utils::debugDir."error-aggiungiIndirizzo.debug", $errors);
+    }
+    else{
+        $idindirizzo=$dbh->lastInsertId("pe.soggetti_id_seq");
+    }
+    
+    if ($errors){
+        //$dbh->rollBack();
+        $result=Array("success"=>"-1","message"=>$errors[2]);
+    }
+    else{
+        $result = Array("success"=>1,"message"=>"OK","indirizzo"=>$idindirizzo);
+    }
     return $result;
 }
 function rimuoviIndirizzo($pratica,$indirizzo){
