34b7d40a00010186487382f460278c1f935baf67
mamogmx <marco.carbone@gmx.com>
2015-03-08 17:38:06 +0100

    added some debug

--- lib/savona.ws.class.php
+++ lib/savona.ws.class.php
@@ -2,7 +2,14 @@
 
 require_once "ws.class.php";
 class wsApp extends ws{
-    
+    function __construct($dsn,$pr=NULL,$params) {
+        self::$dbh = new PDO($dsn);
+        self::$result = Array("success"=>NULL,"message"=>NULL,"id"=>NULL,"time"=>NULL);
+        self::$pratica=$pr;
+        self::$projectParams = $params;
+        self::debug(self::debugDir."PARAMETRI.debug",self::$projectParams);
+        self::init();
+    }
     function init(){
         $dir = Array("data","savona","pe","praticaweb","documenti","pe");
         self::$baseDir = DIRECTORY_SEPARATOR.implode(DIRECTORY_SEPARATOR,$dir).DIRECTORY_SEPARATOR;
